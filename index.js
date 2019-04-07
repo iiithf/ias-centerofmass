1fa1b948e488aeeb5a2a4299f981b13226ef1f4c
Subhajit Sahu <wolfram77@gmail.com>
2019-04-08 01:56:15 +0530

    save

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const WebSocket = require('ws');
 const E = process.env;
 const PORT = parseInt(E['PORT']||'8500', 10);
 const INPUT = E['INPUT']||'ws://192.168.1.7:8000';
+const MODEL = E['MODEL']||'http://192.168.1.7:8501';
 const ws = new WebSocket(INPUT);
 
 
 
 ws.onmessage = (e) => {
-  console.log(e);
+  var balls = JSON.parse(e.data).values;
+  console.log(balls);
 };
 
 

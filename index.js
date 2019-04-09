f025b96d8c691985edaf369e48471aa31eb6f8bd
Subhajit Sahu <wolfram77@gmail.com>
2019-04-10 00:22:06 +0530

    :mushroom: updated code, but yet to test

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const needle = require('needle');
 
 
 const E = process.env;
-const PORT = parseInt(E['PORT']||'8500', 10);
 const INPUT = E['INPUT']||'ws://192.168.1.7:8000';
 const MODEL = E['MODEL']||'http://192.168.1.7:8501';
 const MODELURL = MODEL+'/v1/models/model:predict';
 const CONTAINER = E['CONTAINER']||'';
 const ws = new WebSocket(INPUT);
-const INPUTS = 20;
+const INPUTS = 10;
 var ball = null;
 
 
@@ -42,6 +41,8 @@ function ballMapOutputs(outputs, ball) {
   var [x, y] = outputs, b = ball;
   b.x = b.x+Math.max(x-b.x, 0.1);
   b.y = b.y+Math.max(y-b.y, 0.1);
+  b.x = Math.max(0, Math.min(b.x, 1));
+  b.y = Math.max(0, Math.min(b.y, 1));
   return b;
 }
 
@@ -53,15 +54,18 @@ function ballSetup() {
 
 
 async function modelRun(inputs) {
-  var res = await needle('post', MODELURL, {examples: [{inputs}]}, {json: true});
-  console.log(res.body);
-  return res.body.outputs;
+  var res = await needle('post', MODELURL, {inputs: [inputs]}, {json: true});
+  return res.body.outputs[0];
+}
+
+function onRecieveUpdate(balls) {
+  var inputs = inputsMapBalls(balls);
+  var outputs = modelRun(inputs);
+  ball = ballMapOutputs(outputs, ball);
+  ws.send({type: 'update', data: ball});
 }
 
 
-ws.onmessage = (e) => {
-  var balls = JSON.parse(e.data).values;
-  console.log(balls);
-};
 
-
+ws.onmessage = (e) => onRecieveUpdate(e.data);;
+ballSetup();
